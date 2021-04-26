8d5ee9cdc64d87a729c90ba1a2b3cd467cec6c8a
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-04-26 15:51:26 +0200

    Restructuring createPatient methode

--- src/main/java/stud/ntnu/IDATT2001/MappeDel2/PatientDialog.java
+++ src/main/java/stud/ntnu/IDATT2001/MappeDel2/PatientDialog.java
@@ -3,6 +3,7 @@ package stud.ntnu.IDATT2001.MappeDel2;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.input.GestureEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -10,13 +11,15 @@ import javafx.scene.paint.Color;
 public class PatientDialog extends Dialog<Patient> {
     final String textRegex = "^[a-zA-Z ,.'-]*$";
 
-    public enum Mode {
-        NEW, EDIT, INFO
-    }
+    private Label ssnError = new Label();
 
     private final Mode mode;
 
     private Patient existingPatient = null;
+
+    public enum Mode {
+        NEW, EDIT, INFO
+    }
 
     public PatientDialog() {
         super();
@@ -35,11 +38,6 @@ public class PatientDialog extends Dialog<Patient> {
         createPatient();
     }
 
-    private Label ssnError = new Label();
-
-
-
-
     private void validateTextField(TextField textField, String regex){
         textField.setTextFormatter(new TextFormatter<>(change ->
                 (change.getControlNewText().matches(regex)) ? change : null));
@@ -56,7 +54,58 @@ public class PatientDialog extends Dialog<Patient> {
         });
     }
 
+    private void validateNumber(TextField textField, String regex1, String regex2){
+        textField.setTextFormatter(new TextFormatter<>(change ->
+                (change.getControlNewText().matches(regex1)) ? change : null));
+        textField.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (textField.getText().matches(regex2)){
+                    textField.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,BorderWidths.DEFAULT)));
+                    ssnError.setText("");
+                }
+                else {
+                    textField.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,BorderWidths.DEFAULT)));
+                    ssnError.setTextFill(Color.RED);
+                    ssnError.setText("*please provide 11 digits.");
+                }
+            }
+        });
+    }
+
     private void createPatient() {
+        GridPane grid = setupDialogBox();
+
+        TextField firstName = new TextField();
+        firstName.setPromptText("Ex. Isaac");
+        validateTextField(firstName,textRegex);
+
+        TextField lastName = new TextField();
+        lastName.setPromptText("Ex. Newton");
+        validateTextField(lastName, textRegex);
+
+        TextField socialSecurityNumber = new TextField();
+        socialSecurityNumber.setPromptText("Ex. 12345678912");
+        validateNumber(socialSecurityNumber,"[0-9]{0,11}","[0-9]{11}");
+
+        TextField generalPractitioner = new TextField();
+        generalPractitioner.setPromptText("Ex. Dr. Christina Yang");
+        validateTextField(generalPractitioner,textRegex);
+
+        TextField diagnosis = new TextField();
+        diagnosis.setPromptText("Ex. Artery disease");
+        validateTextField(diagnosis,textRegex);
+
+        infoAndEditSeparator(socialSecurityNumber,firstName,lastName,generalPractitioner,diagnosis);
+
+        setupGrid(grid,socialSecurityNumber,firstName,lastName,generalPractitioner,diagnosis);
+
+        getDialogPane().setContent(grid);
+
+        addAndEditSeparator(socialSecurityNumber,firstName,lastName,generalPractitioner,diagnosis);
+    }
+
+    private GridPane setupDialogBox(){
         // Set title depending upon mode...
         switch (this.mode) {
             case EDIT:
@@ -77,93 +126,20 @@ public class PatientDialog extends Dialog<Patient> {
             default:
                 setTitle("Patient Details - UNKNOWN MODE...");
                 break;
-
         }
 
         GridPane grid = new GridPane();
         grid.setHgap(30);
         grid.setVgap(10);
         grid.setPadding(new Insets(20, 150, 10, 10));
+        return  grid;
+    }
 
-        TextField firstName = new TextField();
-        firstName.setPromptText("Ex. Isaac");
-        validateTextField(firstName,textRegex);
-
-        TextField lastName = new TextField();
-        lastName.setPromptText("Ex. Newton");
-        validateTextField(lastName, textRegex);
-
-        TextField socialSecurityNumber = new TextField();
-        socialSecurityNumber.setPromptText("Ex. 12345678912");
-        socialSecurityNumber.setTextFormatter(new TextFormatter<>(change ->
-                (change.getControlNewText().matches("[0-9]{0,11}")) ? change : null));
-        socialSecurityNumber.setOnKeyTyped(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                if (socialSecurityNumber.getText().matches("[0-9]{11}")){
-                    socialSecurityNumber.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,BorderWidths.DEFAULT)));
-                    ssnError.setText("");
-                }
-                else {
-                    socialSecurityNumber.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,BorderWidths.DEFAULT)));
-                    ssnError.setTextFill(Color.RED);
-                    ssnError.setText("*please provide 11 digits.");
-                }
-            }
-        });
-
-        TextField generalPractitioner = new TextField();
-        generalPractitioner.setPromptText("Ex. Dr. Christina Yang");
-        validateTextField(generalPractitioner,textRegex);
-
-        TextField diagnosis = new TextField();
-        diagnosis.setPromptText("Ex. Artery disease");
-        validateTextField(diagnosis,textRegex);
-
-
-        // Fill inn data from the provided Newspaper, if not null.
-        if ((mode == Mode.EDIT) || (mode == Mode.INFO)) {
-            firstName.setText(existingPatient.getFirstName());
-            lastName.setText(existingPatient.getLastName());
-            socialSecurityNumber.setText(existingPatient.getSocialSecurityNumber());
-            generalPractitioner.setText(existingPatient.getGeneralPractitioner());
-            diagnosis.setText(existingPatient.getDiagnosis());
-            socialSecurityNumber.setText(existingPatient.getSocialSecurityNumber());
-            // Set to non-editable if Mode.INFO
-            if (mode == Mode.INFO) {
-                firstName.setEditable(false);
-                lastName.setEditable(false);
-                socialSecurityNumber.setEditable(false);
-                generalPractitioner.setEditable(false);
-                diagnosis.setEditable(false);
-            }
-        }
-
-        grid.add(new Label("First name"), 0, 0);
-        grid.add(firstName, 1, 0);
-
-
-        grid.add(new Label("Last name"), 0, 2);
-        grid.add(lastName, 1, 2);
-
-        grid.add(new Label("Social security number"), 0, 3);
-        grid.add(socialSecurityNumber, 1, 3);
-
-        grid.add(ssnError,1,4);
-
-        grid.add(new Label("General Practitioner"), 0, 5);
-        grid.add(generalPractitioner, 1, 5);
-
-        grid.add(new Label("Diagnosis"), 0, 7);
-        grid.add(diagnosis, 1, 7);
-
-        Label required = new Label("*Required to fill all fields");
-        required.setTextFill(Color.RED);
-        grid.add(required,0,9);
-
-
-        getDialogPane().setContent(grid);
-
+    private void addAndEditSeparator(TextField socialSecurityNumber,
+                                     TextField firstName,
+                                     TextField lastName,
+                                     TextField generalPractitioner,
+                                     TextField diagnosis){
         setResultConverter((ButtonType button) -> {
             Patient result = null;
             if (button == ButtonType.OK) {
@@ -183,6 +159,57 @@ public class PatientDialog extends Dialog<Patient> {
         });
     }
 
+    private void infoAndEditSeparator(TextField socialSecurityNumber,
+                                      TextField firstName,
+                                      TextField lastName,
+                                      TextField generalPractitioner,
+                                      TextField diagnosis){
+        // Fill inn data from the provided Newspaper, if not null.
+        if ((mode == Mode.EDIT) || (mode == Mode.INFO)) {
+            firstName.setText(existingPatient.getFirstName());
+            lastName.setText(existingPatient.getLastName());
+            socialSecurityNumber.setText(existingPatient.getSocialSecurityNumber());
+            generalPractitioner.setText(existingPatient.getGeneralPractitioner());
+            diagnosis.setText(existingPatient.getDiagnosis());
+            socialSecurityNumber.setText(existingPatient.getSocialSecurityNumber());
+            // Set to non-editable if Mode.INFO
+            if (mode == Mode.INFO) {
+                firstName.setEditable(false);
+                lastName.setEditable(false);
+                socialSecurityNumber.setEditable(false);
+                generalPractitioner.setEditable(false);
+                diagnosis.setEditable(false);
+            }
+        }
+    }
+
+    private void setupGrid(GridPane grid,
+                           TextField socialSecurityNumber,
+                           TextField firstName,
+                           TextField lastName,
+                           TextField generalPractitioner,
+                           TextField diagnosis){
+        grid.add(new Label("First name"), 0, 0);
+        grid.add(firstName, 1, 0);
+
+        grid.add(new Label("Last name"), 0, 2);
+        grid.add(lastName, 1, 2);
+
+        grid.add(new Label("Social security number"), 0, 3);
+        grid.add(socialSecurityNumber, 1, 3);
+
+        grid.add(ssnError,1,4);
+
+        grid.add(new Label("General Practitioner"), 0, 5);
+        grid.add(generalPractitioner, 1, 5);
+
+        grid.add(new Label("Diagnosis"), 0, 7);
+        grid.add(diagnosis, 1, 7);
+
+        Label required = new Label("*Required to fill all fields");
+        required.setTextFill(Color.RED);
+        grid.add(required,0,9);
+    }
 }
 
 
